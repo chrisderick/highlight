b4beb74069f290c76b6bbaba5e2164133ee050ed
Christhyan Derick Dias Barbosa <christhyan.barbosa@cg.df.gov.br>
2019-10-04 17:24:45 -0300

    Criação da pasta

---
+++ src/main/java/com/udfdevs/highlight/HighlightApplication.java
@@ -0,0 +1,13 @@
+package com.udfdevs.highlight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighlightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighlightApplication.class, args);
+	}
+
+}
